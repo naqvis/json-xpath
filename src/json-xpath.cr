d3c0476f4601eff129c7e42bd3c85f3eb46b2845
Ali Naqvi <syed.alinaqvi@gmail.com>
2020-06-11 22:31:30 +0800

    Updated for Crystal 0.35.0

--- src/json-xpath.cr
+++ src/json-xpath.cr
@@ -1,6 +1,6 @@
 # XPath query package for JSON document, lets you extract data from JSON documents through an XPath expression.
 module JSONXPath
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 
   # Parses the JSON and returns an instance of `JSONXPath::Node`
   def self.parse(input : String | IO)
